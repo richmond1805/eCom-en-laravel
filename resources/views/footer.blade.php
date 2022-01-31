d60edc8dcb4e62477c802a1d8a90b77bc7035792
richmond1805 <pascualrichmond26@gmail.com>
2022-01-31 23:29:36 +0800

    login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="card">
+  <div class="card-header">
+    Panel Content
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Panel Footer</li>
+  </ul>
+</div>
